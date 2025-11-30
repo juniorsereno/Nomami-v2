4d16b8ab8b4603821011fe407d878a96728c08b2
Junior Sereno <juniorsereno.br@gmail.com>
2025-11-30 17:43:47 -0300

    att new api

--- nomami-app/app/api/test-env/route.ts
+++ nomami-app/app/api/test-env/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from 'next/server';
 import { env } from 'process';
-env.ASAAS_API_KEY = "$aact_prod_000MzkwODA2MWY2OGM3MWRlMDU2NWM3MzJlNzZmNGZhZGY6OmQ3YjdiMTBmLWJkNTQtNDE5ZS04ZTJmLTRlNDVjYzZkMjI5ZTo6JGFhY2hfNDkxNDIxNzYtYjBkNS00MDFhLWI4MWYtZTkxOTE1MmNjNDcx";
 
 export async function GET() {
   const apiKey = process.env.ASAAS_API_KEY;
