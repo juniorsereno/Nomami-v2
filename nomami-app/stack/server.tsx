c94e1195e1d47de3ff9ff24dde9f5fff60d55416
Junior Sereno <juniorsereno.br@gmail.com>
2025-12-01 16:43:09 -0300

    key build

--- nomami-app/stack/server.tsx
+++ nomami-app/stack/server.tsx
@@ -4,6 +4,6 @@ import { StackServerApp } from "@stackframe/stack";
 
 export const stackServerApp = new StackServerApp({
   tokenStore: "nextjs-cookie",
-  projectId: process.env.NEXT_PUBLIC_STACK_PROJECT_ID,
-  secretServerKey: process.env.STACK_SECRET_SERVER_KEY,
+  projectId: process.env.NEXT_PUBLIC_STACK_PROJECT_ID || 'dummy-project-id-for-build',
+  secretServerKey: process.env.STACK_SECRET_SERVER_KEY || 'dummy-server-key-for-build',
 });
